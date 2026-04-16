16b4447df5af26a59597c68c9ace13129a0f69e4
RUICHIEN <s1135196@pu.edu.tw>
2026-04-16 11:53:44 +0800

    4/16
    檔案更新修正

--- read3.py
+++ read3.py
@@ -7,11 +7,20 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-keyword = '楊'
+keyword = input("請輸入老師姓名關鍵字：")
 
 docs = db.collection("靜宜資管2026B").get() 
 
+found = False  # 用來判斷有沒有找到
+
 for doc in docs:
     teacher = doc.to_dict()
-    if 'name' in teacher and keyword in teacher['name']:
-        print(teacher)
+    if 'name' in teacher and teacher['name'] and keyword in teacher['name']:
+        name = teacher.get('name', '無資料')
+        lab = teacher.get('lab', '無研究室資料')
+
+        print(f"姓名：{name}，研究室：{lab}")
+        found = True
+
+if not found:
+    print("抱歉，查無符合的老師資料")
