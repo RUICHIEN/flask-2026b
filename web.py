a7560fb8312eea2c59d03cdf653015befa93552a
RUICHIEN <s1135196@pu.edu.tw>
2026-05-14 11:27:07 +0800

    webhoook2

--- web.py
+++ web.py
@@ -59,8 +59,8 @@ def webhook():
     # build a request object
     req = request.get_json(force=True)
     # fetch queryResult from json
-    action =  req.["queryResult"]["action"]
-    msg =  req.["queryResult"]["queryText"]
+    action =  req["queryResult"]["action"]
+    msg =  req["queryResult"]["queryText"]
     info = "這是睿謙的機器人，動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
 
