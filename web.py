a8dbbb177733298feb8968de09eb16b04c17aaeb
RUICHIEN <s1135196@pu.edu.tw>
2026-05-28 10:48:35 +0800

    0528第三次

--- web.py
+++ web.py
@@ -34,7 +34,7 @@ db = init_firebase()
 
 app = Flask(__name__)
 
-client = genai.client()
+client = genai.Client()
 
 print("FIREBASE_CONFIG exists:", os.getenv("FIREBASE_CONFIG") is not None)
 # https://jsoneditoronline.org/#right=local.matizi&left=local.monusi
